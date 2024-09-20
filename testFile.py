9eae04a6a24a29aa5493a4d752de1fe617d3e021
czlight <alejandro.r1212@gmail.com>
2024-09-20 19:12:52 +0000

    Fri, Sep 20, 2024, 2:12 PM -05:00

--- testFile.py
+++ testFile.py
@@ -1,4 +1,5 @@
 from crossword import *
+from generate import *
 
 variable1 = Variable(0,0, Variable.DOWN, 5)
 variable2 = Variable(0,0, Variable.DOWN, 5)
@@ -6,4 +7,6 @@ variable2 = Variable(0,0, Variable.DOWN, 5)
 print(hash(variable1))
 print(str(variable1))
 
-CrosswordCreator.revise(variable1, variable1)
+myCrossword = CrosswordCreator()
+
+myCrossword.revise(variable1, variable1)
