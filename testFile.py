e428abaa5ac82b37ab50eb1cb4b6b878fcd6c498
czlight <alejandro.r1212@gmail.com>
2024-09-20 19:14:01 +0000

    Fri, Sep 20, 2024, 2:14 PM -05:00

--- testFile.py
+++ testFile.py
@@ -7,6 +7,31 @@ variable2 = Variable(0,0, Variable.DOWN, 5)
 print(hash(variable1))
 print(str(variable1))
 
-myCrossword = CrosswordCreator()
 
-myCrossword.revise(variable1, variable1)
+def main():
+
+    # Check usage
+    if len(sys.argv) not in [3, 4]:
+        sys.exit("Usage: python generate.py structure words [output]")
+
+    # Parse command-line arguments
+    structure = sys.argv[1]
+    words = sys.argv[2]
+    output = sys.argv[3] if len(sys.argv) == 4 else None
+
+    # Generate crossword
+    crossword = Crossword(structure, words)
+    creator = CrosswordCreator(crossword)
+    assignment = creator.solve()
+
+    # Print result
+    if assignment is None:
+        print("No solution.")
+    else:
+        creator.print(assignment)
+        if output:
+            creator.save(assignment, output)
+
+
+if __name__ == "__main__":
+    main()
