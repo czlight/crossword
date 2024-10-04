d37701415d5aa35bbffa4de40014d5afce60514d
czlight <alejandro.r1212@gmail.com>
2024-10-04 05:22:22 +0000

    Fri, Oct 4, 2024, 12:22 AM -05:00

--- generate.py
+++ generate.py
@@ -163,10 +163,11 @@ class CrosswordCreator():
                 for yvalue in self.domains[y]:
                     print("checking indices of xvalue, yvalue", xvalue, yvalue)
                     if xvalue[overlapIndices[0]] == yvalue[overlapIndices[1]]:
+                        continue
                         # this value in x's domain has a possible value in y's domain
                         print("x has a value in its domain that has a possible value in y's domain")
                         print("these values are: " ,xvalue, yvalue)
-                    
+
 
 
 
