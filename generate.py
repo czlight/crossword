9129efbf84bb915c8896f36490ebdbc4e7891a37
czlight <alejandro.r1212@gmail.com>
2024-10-04 05:32:33 +0000

    Fri, Oct 4, 2024, 12:32 AM -05:00

--- generate.py
+++ generate.py
@@ -162,8 +162,10 @@ class CrosswordCreator():
             print("there's overlap!")
             print("overlapIndices is:", overlapIndices)
             for xvalue in self.domains[x]:
-
+                iterationCount = 0
                 for yvalue in self.domains[y]:
+                    iterationCount += 1
+                    print("iteration count:", iterationCount)
                     print("checking indices of xvalue, yvalue", xvalue, yvalue)
                     if xvalue[overlapIndices[0]] == yvalue[overlapIndices[1]]:
                         # this value in x's domain has a value in y's
@@ -171,6 +173,10 @@ class CrosswordCreator():
                         print("x has a value in its domain that has a possible value in y's domain")
                         print("these values are: " ,xvalue, yvalue)
                         break
+                    if iterationCount == yDomainLength:
+                        # no possible value in y's domain found
+                        # remove this from x's domain
+                        arcRemovalSet.add(xvalue)
 
 
 
